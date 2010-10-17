649e4f5c0dc5e552b4091a9aa4a0a46e638a9707
Kazuhiro Osawa <yappo@shibuya.pl>
2010-10-17 18:37:51 +0900

    added twitter irc gateway for anysan

---
+++ eg/tig-for-anysan.pl
@@ -0,0 +1,52 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use lib 'lib';
+use utf8;
+
+use AnyEvent::IRC::Server;
+use AnySan::Provider::Twitter;
+use Config::Pit;
+use Encode;
+
+my $config = pit_get('tig-for-anysan', require => {
+    consumer_key    => 'your twitter consumer_key',
+    consumer_secret => 'your twitter consumer_secret',
+    token           => 'your twitter access_token',
+    token_secret    => 'your twitter access_token_secret',
+});
+
+my $twitter = twitter
+    %{ $config },
+    method          => 'userstream',
+    ;
+
+my $ircd = AnyEvent::IRC::Server->new(
+    port       => 6667,
+    servername => '127.0.0.1',
+);
+$ircd->run();
+
+$ircd->reg_cb(
+    daemon_privmsg => sub {
+        my ($irc, $nick, $chan, $text) = @_;
+        $twitter->send_message(decode( utf8 => $text ));
+    },
+);
+
+AnySan->register_listener(
+    tig => {
+        event => 'timeline',
+        cb => sub {
+            my $receive = shift;
+            return unless $receive->message;
+            $ircd->daemon_cmd_privmsg(
+                $receive->from_nickname => '#twitter',
+                encode( utf8 => $receive->message ),
+            );
+        },
+    },
+);
+
+AnySan->run;
+
